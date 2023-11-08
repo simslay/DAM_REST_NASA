fb13c8ee833936e369f3ab8cfa7195855f662a24
simslay <simon.grandsire@gmail.com>
2023-11-08 16:03:48 +0100

    Initial commit

--- src/main/java/com/example/dam_rest_nasa/HelloController.java
+++ src/main/java/com/example/dam_rest_nasa/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.dam_rest_nasa;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            URL oracle = new URL("https://www.oracle.com");
+            String inputLine;
+            BufferedReader in = new BufferedReader(new InputStreamReader((oracle.openStream())));
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
