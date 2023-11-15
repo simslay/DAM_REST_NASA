1b71c3cc65fd4ce7ae6d9ab4226a90fdf9695e93
simslay <simon.grandsire@gmail.com>
2023-11-15 14:45:37 +0100

    Initial commit

--- src/main/java/com/example/dam_rest_nasa/HelloController.java
+++ src/main/java/com/example/dam_rest_nasa/HelloController.java
@@ -4,11 +4,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
@@ -22,6 +25,25 @@ public class HelloController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+//        oracleExemple();
+        /** Récupération de donnees Rest sous forme de fichier JSON */
+        String apodUrl = "https://api.nasa.gov/planetary/apod";
+        String apiKey = "XJJDhlvsGLcGc0bqi8tWKSUCQPPUz8PTKntxGmNp";
+
+        try {
+            URL connexionSiteNasaApod = new URL(apodUrl + "?api_key=" + apiKey);
+            URLConnection urlConnection = connexionSiteNasaApod.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void oracleExemple() {
         try {
             URL oracle = new URL("https://www.oracle.com");
             String inputLine;
